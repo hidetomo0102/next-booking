bf1e102115b90aaf0d01d312b0d0f941c4de6c93
hidetomo0102 <hidenorishio@icloud.com>
2021-11-30 00:40:20 +0900

    update sendEmail

---
+++ utils/sendEmail.ts
@@ -0,0 +1,37 @@
+import { createTransport, Transporter } from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+import SMTPConnection, {
+  AuthenticationType,
+} from "nodemailer/lib/smtp-connection";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+interface SendEmailOptions {
+  email: string;
+  subject: string;
+  message: string;
+}
+
+const auth: AuthenticationType = {
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASSWORD,
+};
+
+const createTransportOption: SMTPConnection.Options = {
+  host: process.env.SMTP_HOST,
+  port: Number(String(process.env.SMTP_HOST)),
+  auth: auth,
+};
+
+export const sendEmail = async (options: SendEmailOptions): Promise<any> => {
+  const transporter: Transporter<SMTPTransport.SentMessageInfo> =
+    createTransport(createTransportOption);
+
+  const message: Mail.Options = {
+    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(message);
+};
