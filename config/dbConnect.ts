e3e3993ebf81c88ee3c85d980c4674190e3bb19a
土田健斗 <tsuchitakento@tsuchitakentonoMacBook-Pro.local>
2021-12-02 21:34:12 +0900

    fix-config

--- config/dbConnect.ts
+++ config/dbConnect.ts
@@ -1,10 +1,10 @@
 import { connect, connection, Error } from "mongoose";
 
-export const dbConnect = () => {
+export const dbConnect = (): void => {
   if (connection.readyState > 1) {
     return;
   }
-  const uri = process.env.DB_URI;
+  const uri: string | undefined = process.env.DB_URI;
 
   if (!uri) {
     throw new Error("DB_URI is not set");
