c494f36d9eadcc9c86b7d3780f20a8cf915a8cbc
hidetomo0102 <hidenorishio@icloud.com>
2021-12-03 00:22:24 +0900

    fix dbConnect

--- config/dbConnect.ts
+++ config/dbConnect.ts
@@ -4,7 +4,7 @@ export const dbConnect = (): void => {
   if (connection.readyState > 1) {
     return;
   }
-  const uri: string | undefined = process.env.DB_URI;
+  const uri = process.env.DB_URI;
 
   if (!uri) {
     throw new Error("DB_URI is not set");
